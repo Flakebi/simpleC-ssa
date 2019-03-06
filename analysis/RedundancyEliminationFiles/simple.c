272a015d213cf910334ee0411e8a24a8284a5a11
Flakebi <flakebi@t-online.de>
2019-03-07 00:29:49 +0100

    Add example code for redundancy elimination

---
+++ analysis/RedundancyEliminationFiles/simple.c
@@ -0,0 +1,16 @@
+
+int main() {
+    int x = 2;
+    int y = 3;
+    int i;
+    int j;
+    int k;
+    int xx = x * 34;
+    j = k * 5;
+    for (i = 0; i < 4; ++i) {
+        x = y * (x + i + y);
+        y = 3;
+        j = k * 5;
+    }
+    return x + xx + j;
+}
